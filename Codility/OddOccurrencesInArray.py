542810009d79d5646d5c6e6f204fa75c00fda603
Aditya Alif Nugraha <adityaalifnugraha@gmail.com>
2018-01-12 09:56:51 +0700

    bismilah

--- Codility/OddOccurrencesInArray.py
+++ Codility/OddOccurrencesInArray.py
@@ -1,14 +1,20 @@
 def solution(A=[]):
-    pairs = []
-    for i in A:
-        if i not in pairs:
-            pairs.append(i)
-        else:
-            pairs.remove(i)
-    if len(pairs) == 0:
-        return 0
-    else:
-        return pairs[0]
+    # pairs = []
+    # for i in A:
+    #     if i not in pairs:
+    #         pairs.append(i)
+    #     else:
+    #         pairs.remove(i)
+    # if len(pairs) == 0:
+    #     return 0
+    # else:
+    #     return pairs[0]
+    result = 0
+    for number in A:
+        result ^= number
+        print(result)
+ 
+    return result
 
 
 print(solution([9, 3, 9, 3, 9, 7, 9]))
