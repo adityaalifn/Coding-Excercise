9b51fd323335e0941cecab5000da7f8f9ea64b1c
Aditya Alif Nugraha <adityaalifnugraha@gmail.com>
2018-01-16 12:26:56 +0700

    fix solution

--- Codility/PermMissingElem.py
+++ Codility/PermMissingElem.py
@@ -1,6 +1,6 @@
 def solution(A=[]):
     A_len = len(A) + 1
-    val = (A_len * (A_len + 1)) / 2
-    return int(val - sum(A))
+    val = (A_len * (A_len + 1)) // 2
+    return (val - sum(A))
 
 print(solution([2,3,1,5]))
