d222504610bf788b83e296d81ac781d8602a317e
Aditya Alif Nugraha <adityaalifnugraha@gmail.com>
2018-01-18 18:44:50 +0700

    fix solution

--- Codility/PermCheck.go
+++ Codility/PermCheck.go
@@ -11,20 +11,14 @@ func main() {
 }
 
 func Solution(A []int) int {
-	// n := len(A)
-	// result := n * (n + 1) / 2
-	// count := 0
-	// for _, angka := range A {
-	// 	count += angka
-	// }
+	n := len(A)
+	result := n * (n + 1) / 2
+	count := 0
+	for _, angka := range A {
+		count += angka
+	}
 
-	// if count == result {
-	// 	return 1
-	// }
-	// return 0
-	A = sort.Ints(A)
-	list := make([]int, )
-	if A == range(1, len(A)+1){
+	if count == result {
 		return 1
 	}
 	return 0
