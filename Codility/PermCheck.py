30f6bbd16f934de39de860546797abb8a0230489
Aditya Alif Nugraha <adityaalifnugraha@gmail.com>
2018-01-19 14:15:48 +0700

    fix solution

--- Codility/PermCheck.py
+++ Codility/PermCheck.py
@@ -1,9 +1,8 @@
 def solution(A=[]):
     A.sort()
-    if A == [i for i in range(1,len(A)+1)]:
+    if A == [i for i in range(1, len(A) + 1)]:
         return 1
     return 0
-    
 
 
-print(solution([4,1,3,2]))
+print(solution([4, 1, 3, 2]))
