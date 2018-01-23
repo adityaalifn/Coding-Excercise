a73e14016b44c2d8d537f412bd5e652310d86d2c
Aditya Alif Nugraha <adityaalifnugraha@gmail.com>
2018-01-23 09:20:45 +0700

    add MissingInteger solution

---
+++ Codility/MissingInteger.py
@@ -0,0 +1,12 @@
+def solution(A=[]):
+    # n = len(A)
+    # count = [0] * (n+1)
+    # for i in range(n):
+    #     count[A[i]] += 1
+    # return A.pop(count.index(min(count)))
+    for i in range(len(A)):
+        A[i]
+
+
+print(solution([1, 3, 6, 4, 1, 2]))
+print(solution([1, 2, 3]))
