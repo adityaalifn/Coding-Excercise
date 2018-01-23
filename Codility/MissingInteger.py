2f86d16ddd2f5b904f39877525d2765ebb087e25
Aditya Alif Nugraha <adityaalifnugraha@gmail.com>
2018-01-23 09:26:34 +0700

    fix MissingInteger solution

--- Codility/MissingInteger.py
+++ Codility/MissingInteger.py
@@ -4,9 +4,14 @@ def solution(A=[]):
     # for i in range(n):
     #     count[A[i]] += 1
     # return A.pop(count.index(min(count)))
-    for i in range(len(A)):
-        A[i]
+    A.sort()
+    min = 1
+    for elem in A:
+        if elem == min:
+            min+=1
+    return min
 
 
 print(solution([1, 3, 6, 4, 1, 2]))
 print(solution([1, 2, 3]))
+print(solution([-1, 2, 3]))
